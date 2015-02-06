f3e9b6bc9bcb0d165b3305bd66be3511f2a8ac42
Julian Veling <julianveling@Julians-MacBook-Pro.local>
2015-02-06 19:02:39 +0000

    added weather functions and tests

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,5 +1,16 @@
 module Weather
 
-  
-  
+  weather=nil
+
+  def weather_test(prob) 
+    prob < 3 ? weather = "stormy" : weather = "sunny"
+    weather
+  end
+
+  def weather_prob
+    prob = [1,2,3,5,6,7,8,9,10].sample
+    weather_test(prob)
+    prob
+  end  
+
 end  
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,7 +1,18 @@
 require 'weather'
 
+class Airport 
+end 
+
 describe Weather do
+  let(:gatwick){Airport.new}
+  it 'weather_prob method returns a number between 1 and 10' do
+    expect(gatwick.weather_prob).to be<=10 and be>=1
+  end
 
+  it 'weather is stormy or sunny depending on the prob method return value' do  
+    expect(gatwick.weather_test(3)).to eq('sunny')
+    expect(gatwick.weather_test(2)).to eq('stormy')
+  end    
   
-
 end
+    
