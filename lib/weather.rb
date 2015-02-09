79d3f4db84c1527adfb102160d160a9cb4522c68
Julian Veling <julianveling@Julians-MacBook-Pro.local>
2015-02-09 14:44:48 +0000

    refactored code to include boolean for stormy weather, and tests to allow control of the stormy attribute

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,13 +1,7 @@
 module Weather
 
-  def weather_prob
-    prob = [1,2,3,4,5,6,7,8,9,10].sample
-  end  
-
-  def weather_test(prob) 
-    weather=nil
-    prob < 4 ? weather = "stormy" : weather = "sunny"
-    weather
+  def stormy?
+    Array(1..10).sample < 3
   end
 
 end  
