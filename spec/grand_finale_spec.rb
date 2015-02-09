053fce6228eb402c10389276a147819d7ae2b948
veliancreate <julian_veling@hotmail.com>
2015-02-09 20:12:23 +0000

    added grande finale finale

--- spec/grand_finale_spec.rb
+++ spec/grand_finale_spec.rb
@@ -29,4 +29,32 @@ context 'grand finale' do
     expect(airport.plane_count).to eq(6)
   end  
 
+  it 'each plane should have a landed status' do
+
+    planes.each do |p|
+      expect(p).to be_flying
+    end  
+
+  end  
+
+  it 'each plane should be able to take off' do
+
+    planes.each do |p|
+      while airport.plane_count !=0
+        begin
+          airport.plane_land(p)
+        rescue
+          next
+        end
+      end
+      expect(airport.plane_count).to eq(0)
+    end  
+
+  end
+
+  it 'each plane should have a flying status' do
+    planes.each do |p|
+      expect(p).to_not be_flying
+    end  
+  end  
 end  
